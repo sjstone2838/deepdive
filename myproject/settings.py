d957552353345f84e9acb99183b99dec3cd624e2
Sam Stone <sjstone1987@gmail.com>
2015-10-31 18:17:27 -0400

    commit5

--- myproject/settings.py
+++ myproject/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
-        'TEMPLATE_DEBUG':True,
+        #'TEMPLATE_DEBUG':True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
