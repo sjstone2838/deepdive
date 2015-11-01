04832758dd584ba9792dee309687df1771005ec0
Sam Stone <sjstone1987@gmail.com>
2015-11-01 16:13:14 -0500

    Commit21

--- myproject/settings.py
+++ myproject/settings.py
@@ -110,11 +110,11 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = 'staticfiles'
+#STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
+#STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
