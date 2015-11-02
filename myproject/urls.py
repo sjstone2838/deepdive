75dc3687111dd7e2189dc6b3dd9759d0a986bde3
Sam Stone <sjstone1987@gmail.com>
2015-11-02 18:15:36 -0500

    commit29

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
