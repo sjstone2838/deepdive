4919a10bfcac8c3176e589375de55fb4b598c47d
Sam Stone <sjstone1987@gmail.com>
2015-11-02 18:46:23 -0500

    commit32

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles/')
 STATIC_URL = '/static/'
 """
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
     #added - take out?
     os.path.join(BASE_DIR, 'staticfiles'),
     'myproject/lessons/static',
