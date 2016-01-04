54bab2a7f89ab0277dcc259bbe586c57289a7c40
Sam Stone <sjstone1987@gmail.com>
2016-01-04 12:47:18 +0530

    New CKEditor

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -98,7 +99,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
